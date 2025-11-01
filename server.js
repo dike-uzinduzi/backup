8724d87c49c9b21f5a2eaac1662142ffef0c1f6f
Dike Mahoko <dike@uzinduziafrica.com>
2025-11-02 00:24:38 +0200

    Pese and env
    imported pesepay
    added env file

--- server.js
+++ server.js
@@ -1,5 +1,42 @@
 var http = require('http');
- 
+const { Pesepay } = require('pesepay')
+const {loadEnvFile} = require('node:process');
+
+loadEnvFile();
+
+const pesepay = new Pesepay(process.env.INTEGRATION_KEY, process.env.ENCRYPTION_KEY);
+
+pesepay.resultUrl = process.env.RESULT_URL;
+pesepay.returnUrl = process.env.RETURN_URL;
+
+const transaction = pesepay.createTransaction(amount, 'CURRENCY_CODE', 'PAYMENT_REASON')
+
+pesepay
+    .initiateTransaction(transaction)
+    .then((response) => {
+      // User the redirect url to complete the transaction on Pesepay payment page
+      const redirectUrl = response.redirectUrl;
+      // Save the reference number (used to check the status of a transaction and to make the payment)
+      const referenceNumber = response.referenceNumber;
+    })
+    .catch((error) => {
+     console.error('Error initiating transaction:', error);
+  });
+
+const payment = pesepay.createPayment('CURRENCY_CODE', 'PAYMENT_METHOD_CODE', 'CUSTOMER_EMAIL(OPTIONAL)', 'CUSTOMER_PHONE_NUMBER(OPTIONAL)', 'CUSTOMER_NAME(OPTIONAL)')
+
+const requiredFields = {'requiredFieldName': 'requiredFieldValue'}
+
+pesepay
+     .makeSeamlessPayment(payment, 'PAYMENT_REASON', AMOUNT, requiredFields)
+     .then((response) => {
+       // Save the poll url and reference number (used to check the status of a transaction)
+       const pollUrl = response.pollUrl;
+       const referenceNumber = response.referenceNumber;
+     })
+     .catch((error) => {
+       console.error('Error making seamless payment:', error);
+     });
 http.createServer(function (req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/html'});
